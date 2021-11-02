9c073db35416f3a828810231cebf55d4493e1818
PoSungKim <psungkr94@likelion.org>
2021-11-03 02:16:36 +0900

    Demo

--- src/main/java/com/fintech/chatbot_spring/Config/WebConfig.java
+++ src/main/java/com/fintech/chatbot_spring/Config/WebConfig.java
@@ -1,34 +1,34 @@
-package com.fintech.chatbot_spring.Config;
+// package com.fintech.chatbot_spring.Config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.CorsConfigurationSource;
+// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    // @Override
-    // public void addCorsMappings(CorsRegistry registry) {
-    //     registry.addMapping("/**")
-    //             .allowedOrigins("*")
-    //             .allowedMethods("GET", "POST")
-    //             .maxAge(3000);
-    // }
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
+//     // @Override
+//     // public void addCorsMappings(CorsRegistry registry) {
+//     //     registry.addMapping("/**")
+//     //             .allowedOrigins("*")
+//     //             .allowedMethods("GET", "POST")
+//     //             .maxAge(3000);
+//     // }
+//     @Bean
+//     public CorsConfigurationSource corsConfigurationSource() {
+//         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("http://localhost:81");
-        configuration.addAllowedOrigin("https://posungkim.github.io/chatbot");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true);
+//         configuration.addAllowedOrigin("http://localhost:81");
+//         configuration.addAllowedOrigin("https://posungkim.github.io/chatbot");
+//         configuration.addAllowedHeader("*");
+//         configuration.addAllowedMethod("*");
+//         configuration.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-}
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         source.registerCorsConfiguration("/**", configuration);
+//         return source;
+//     }
+// }
