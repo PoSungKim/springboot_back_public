7d7f674a20ae93170d34076b11f6b48803837488
PoSungKim <psungkr94@likelion.org>
2021-12-06 23:32:17 +0900

    AWS EC2 Kafka 연동 (Producer, Consumer 모두 가능)2

--- src/main/java/com/fintech/chatbot_spring/Config/WebSocketConfig.java
+++ src/main/java/com/fintech/chatbot_spring/Config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocketConnection")
-                .setAllowedOrigins("http://localhost:81", "https://posungkim.github.io", "http://ec2-13-124-198-75.ap-northeast-2.compute.amazonaws.com")
+                .setAllowedOrigins("http://localhost:81", "https://posungkim.github.io", "ip-172-31-1-81.ap-northeast-2.compute.internal")
                 .withSockJS();
     }
     @Override
--- src/main/java/com/fintech/chatbot_spring/Service/KafkaProducerService.java
+++ src/main/java/com/fintech/chatbot_spring/Service/KafkaProducerService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaProducerService {
     private static final String TOPIC = "chatbot";
+    private final KafkaTemplate kafkaTemplate;
 
     @Autowired
     public KafkaProducerService(KafkaTemplate kafkaTemplate) {
