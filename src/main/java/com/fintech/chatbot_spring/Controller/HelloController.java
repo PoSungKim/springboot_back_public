70a60e5ab847ed7d01a5cfa0053cb636431cb896
PoSungKim <psungkr94@likelion.org>
2021-12-06 18:01:19 +0900

    AWS CORS Added

--- src/main/java/com/fintech/chatbot_spring/Controller/HelloController.java
+++ src/main/java/com/fintech/chatbot_spring/Controller/HelloController.java
@@ -12,7 +12,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/hello")
-@CrossOrigin(origins = {"http://localhost:81", "https://posungkim.github.io"}, allowedHeaders = "*", maxAge = 3600)
+@CrossOrigin(origins = {"http://localhost:81", "https://posungkim.github.io", "http://ec2-13-124-198-75.ap-northeast-2.compute.amazonaws.com"}, allowedHeaders = "*", maxAge = 3600)
 public class HelloController {
 
     private final RestService RestService;
