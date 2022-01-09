442bc38f2f1684e86f794ebd1e3cdb5ffb2b395e
Po Sung Kim <psungkr94@korea.ac.kr>
2022-01-09 23:29:02 +0900

    httpBasic 적용 - GET 방식 & POST 방식

--- src/main/java/com/fintech/chatbot_spring/Config/SecurityConfig.java
+++ src/main/java/com/fintech/chatbot_spring/Config/SecurityConfig.java
@@ -14,12 +14,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                //.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
                 .and()
-                .formLogin()
-                ;
+                .httpBasic()
+        ;
     }
 
     @Override
--- src/test/java/com/fintech/chatbot_spring/Config/BasicAuthenticationTest.java
+++ src/test/java/com/fintech/chatbot_spring/Config/BasicAuthenticationTest.java
@@ -2,18 +2,18 @@ package com.fintech.chatbot_spring.Config;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.http.HttpHeaders;
-import java.net.http.HttpResponse;
+import java.net.URI;
 import java.util.Base64;
 
 @SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,14 +26,13 @@ public class BasicAuthenticationTest {
 
     RestTemplate restTemplate = new RestTemplate();
 
-    private String getLoginUrl() {
+    private URI getLoginUrl() {
         return UriComponentsBuilder
                 .fromUriString("http://localhost:" + port)
                 .path("/logintest")
                 .encode()
                 .build()
-                .toUri()
-                .toString();
+                .toUri();
     }
 
     @DisplayName("1. 실패 케이스 - Header에 Base64 Encoding 값 미포함")
@@ -52,10 +51,9 @@ public class BasicAuthenticationTest {
                 "user:user".getBytes()
         ));
 
-        HttpEntity<String> entity = new HttpEntity(null, headers);
-
-        HttpResponse<String> httpResponse = restTemplate.getForObject(getLoginUrl(), String.class);
-        logger.warn(response);
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(getLoginUrl(), HttpMethod.GET, entity, String.class);
+        logger.warn(response.getBody());
     }
 
 }
