d71f91dd5a0797572f1ef0e21ff722ba6e191079
Po Sung Kim <psungkr94@korea.ac.kr>
2022-02-06 15:37:54 +0900

    WebSecurity 특정 URL만 authentication 체크하기5

--- src/main/java/com/fintech/chatbot_spring/Config/SecurityConfig.java
+++ src/main/java/com/fintech/chatbot_spring/Config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
 //                .antMatchers("/", "/swagger*/**").authenticated()
 //                .anyRequest().permitAll()
-                .antMatchers("/").permitAll()
+//                .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
 //                .and()
 //                .formLogin()
