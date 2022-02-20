e07c11c7b61bdf30d22f2acf011cb6f93d8e4513
Po Sung Kim <psungkr94@korea.ac.kr>
2022-02-20 21:28:28 +0900

    UserController CORS 설정 추가

--- src/main/java/com/fintech/chatbot_spring/Controller/UserController.java
+++ src/main/java/com/fintech/chatbot_spring/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.fintech.chatbot_spring.Controller;
 
 import com.fintech.chatbot_spring.Service.UserService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = {"http://localhost:81", "https://posungkim.github.io", "http://ec2-3-35-173-205.ap-northeast-2.compute.amazonaws.com"}, allowedHeaders = "*", maxAge = 3600)
 public class UserController {
 
     private final UserService userService;
