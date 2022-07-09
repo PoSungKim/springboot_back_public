b5038f257cdb0d08b4f06570e0d2fb68ed04a41d
Po Sung Kim <psungkr94@korea.ac.kr>
2022-07-09 18:47:43 +0900

    WebMvc & WebSocket Config 설정 - (1) AllowedOrigins에서 localhost는 80번 포트만 허용

--- src/main/java/com/fintech/chatbot_spring/Config/WebMvcConfig.java
+++ src/main/java/com/fintech/chatbot_spring/Config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:81", "https://posungkim.github.io", "http://43.200.125.247", "http://benebean-lightbulb.com")
+                    .allowedOrigins("http://localhost", "https://posungkim.github.io", "http://43.200.125.247", "http://benebean-lightbulb.com")
                     ;
         }
 }
